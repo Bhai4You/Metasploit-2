de8f27548a7c0162c25a7150b287f0fc3f6e2bb6
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2017-10-09 07:28:00 +0530

    Create bundle.sh

---
+++ bundle.sh
@@ -0,0 +1,39 @@
+
+echo 
+##########################################################
+figlet -f small Step-3
+figlet -f small Install MFS..
+echo 
+##########################################################
+echo 
+##########################################################
+figlet -f big Bhai 4 You
+echo 
+##########################################################
+echo 
+##########################################################
+echo
+##########################################################
+cd $HOME/metasploit-framework
+bundle install -j5
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+rm ./modules/auxiliary/gather/http_pdf_authors.rb
+ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+echo 
+##########################################################
+figlet -f big Thanks
+echo 
+##########################################################
+echo 
+##########################################################
+echo 
+##########################################################
+echo 
+##########################################################
+figlet -f big Bhai 4 You
+echo 
+##########################################################
+figlet -f small SUTARIYA
+figlet -f small PARIXIT
+echo 
+##########################################################
